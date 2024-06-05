fdd530fd2b3f2736c2d504221be1cc94eab285bb
ryan wetmore <ryanwetmore1014@gmil.com>
2024-06-05 12:08:06 -0400

    completed json exercise

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,28 @@ namespace APIsAndJSON
 {
     internal class OpenWeatherMapAPI
     {
+        //
+
+        public static void GetTemp()
+        {
+
+            var appsettingsText = File.ReadAllText("appsettings.json");
+
+            var apiKey = JObject.Parse(appsettingsText).GetValue("key").ToString();
+
+            Console.WriteLine("Enter Zip:");
+
+            var zip = Console.ReadLine();
+
+            var url = $"http://api.openweathermap.org/data/2.5/weather?zip={zip}&appid={apiKey}&units=imperial";
+
+            var client = new HttpClient();
+
+            var jsonText = client.GetStringAsync(url).Result;
+
+            var weatherObj = JObject.Parse(jsonText);
+
+            Console.WriteLine($"Temp: {weatherObj["main"]["temp"]}");
+        }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class RonVSKanyeAPI
     {
+
+        public static void Convo()
+        {
+            var client = new HttpClient();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Kanye says:\n{GetKanyeQuote(client)}\n");
+                Thread.Sleep(2000);
+                Console.WriteLine($"Ron says:\n{GetRonQuote(client)}");
+                Thread.Sleep(2000);
+            }
+        }
+        private static string GetKanyeQuote(HttpClient client)
+        {
+            var jsonText = client.GetStringAsync("https://api.kanye.rest/").Result;
+
+            return JObject.Parse(jsonText).GetValue("quote").ToString();
+
+
+        }
+
+
+        private static string GetRonQuote(HttpClient client)
+        {
+            var jsonText = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+            var quote = jsonText.Replace('[', ' ').Replace(']', ' ').Replace('"', ' ').Trim();
+
+            return quote;
+        }
     }
 }
